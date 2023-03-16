ecee8b9169fc1698fb6348e9c061120daa723077
janekrobel <robelkowo@gmail.com>
2023-03-16 19:22:58 +0100

    kolejny glupi blad

--- server/addAllCoins.js
+++ server/addAllCoins.js
@@ -24,17 +24,20 @@ const options = {
 
 axios.request(options).then(function (response) {
 	coins = response.data.data.coins;
-  console.log(coins);
   coins.forEach((_coin)=>{
     let coin = {
-        "uuid" : _coin.uuid,
-        "price" : _coin.price,
-        "marketCap" : _coin.marketCap,
-        "volume" : _coin["24hVolume"],
-        "change" : _coin.change,
-    }
+      "uuid" : _coin.uuid,
+      "symbol" : _coin.symbol,
+      "name" : _coin.name,
+      "price" : _coin.price,
+      "marketCap" : _coin.marketCap,
+      "iconUrl" : _coin.iconUrl,
+      "volume" : _coin["24hVolume"],
+      "change" : _coin.change,
+  }
 
-    coinController.createCoin(_coin);
+    console.log(coin.volume);
+    coinController.createCoin(coin);
 });
 }).catch(function (error) {
 	console.error(error);
--- server/updateCoins.js
+++ server/updateCoins.js
@@ -30,16 +30,12 @@ const updateCoins = () => {
     console.log(coins);
     coins.forEach((_coin)=>{
       let coin = {
-          "uuid" : _coin.uuid,
-          "symbol" : _coin.symbol,
-          "name" : _coin.name,
-          "price" : _coin.price,
-          "marketCap" : _coin.marketCap,
-          "iconUrl" : _coin.iconUrl,
-          "volume" : _coin["24hVolume"],
-          "change" : _coin.change,
-      }
-  
+        "uuid" : _coin.uuid,
+        "price" : _coin.price,
+        "marketCap" : _coin.marketCap,
+        "volume" : _coin["24hVolume"],
+        "change" : _coin.change,
+    }  
       console.log(coin);
   
       coinController.setCoin(coin);
