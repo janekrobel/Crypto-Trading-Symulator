2931750bea277ed42ab8217a11949cd968d645df
janekrobel <robelkowo@gmail.com>
2023-03-28 18:46:01 +0200

    bledy

---
+++ server/apiControllers/emailController.js
@@ -0,0 +1,30 @@
+const nodeMailer = require("nodemailer");
+
+const sendLoginEmail = async(_to, link) =>{
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.swissmail.org',
+        port: 587,
+        secure: false,
+        auth: {
+          user: 'cryptotradingsymulator@email.ch',
+          pass: 'Robelkowo2017' //add to .env
+        }
+      });
+      
+    const mailOptions = {
+        from: 'cryptotradingsymulator@email.ch',
+        to: _to,
+        subject: 'Complete your login!',
+        text: link
+    };
+      
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+sendLoginEmail("robelkowo@gmail.com", "wp.pl");
--- server/apiControllers/loginController.js
+++ server/apiControllers/loginController.js
@@ -1,10 +1,12 @@
-const nodeMailer = require("nodemailer");
+const jwt = require('jsonwebtoken');
+const emailController = require('./emailController.js');
+
 
 exports.postLogin = (req, res) => {
     const email = req.body.email;
     const token = jwt.sign({ email }, process.env.SECRETKEY);
     const loginLink = `https://localhost:3001/login/link?key=${token}`;
-    sendLoginEmail(email, loginLink).then(() => {
+    emailController.sendLoginEmail(email, loginLink).then(() => {
         res.send(true);
     });
 };
@@ -20,30 +22,3 @@ exports.getLink = (req, res) => {
         res.send("Invalid login link");
     }
 };
-
-const sendLoginEmail = async(_to, link) =>{
-    const transporter = nodeMailer.createTransport({
-        host: 'smtp.swissmail.org',
-        port: 587,
-        secure: false,
-        auth: {
-          user: 'cryptotradingsymulator@email.ch',
-          pass: 'Robelkowo2017' //add to .env
-        }
-      });
-      
-    const mailOptions = {
-        from: 'cryptotradingsymulator@email.ch',
-        to: _to,
-        subject: 'Complete your login!',
-        text: link
-    };
-      
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-    });
-}
