7f4e4d9be3d2ab32d839d7cf858793fab9831ca6
Jan Robel <74827759+janekrobel@users.noreply.github.com>
2023-05-08 11:25:24 +0200

    improved login page

--- server/routings/loginRouting.js
+++ server/routings/loginRouting.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../apiControllers/loginController.js");
+const coinModel = require("../models/coinModel.js");
 
-router.get('/', function(req, res) {
-    res.render('login');
+router.get('/', async function(req, res) {
+    let _coins = await coinModel.getAllCoins() 
+    let slicedCoins = _coins.slice(0,4);
+    res.render('login',{
+        coins: slicedCoins
+    });
 });
 
 router.post('/', controller.postLogin);
